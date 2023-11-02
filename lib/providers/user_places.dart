5cb60f05abf2ffa05bccbd50f437b963a4ffa6fd
Oluwasegun <oluyedis@gmail.com>
2023-11-02 10:04:00 +0100

    added a provider to get new places added by users

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -11,4 +11,6 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
   }
 }
 
-final userPlacesProvider = StateNotifierProvider((ref) => UserPlacesNotifier());
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+        (ref) => UserPlacesNotifier());
