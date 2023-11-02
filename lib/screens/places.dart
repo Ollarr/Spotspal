64fd3e7aff507a6e6335350f17b576eb9fc720c7
Oluwasegun <oluyedis@gmail.com>
2023-11-02 10:04:37 +0100

    added a provider to get new places added by users

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spotspal/providers/user_places.dart';
 import 'package:spotspal/screens/add_place.dart';
 import 'package:spotspal/widgets/places_list.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("My favourite places"),
@@ -22,7 +26,7 @@ class PlacesScreen extends StatelessWidget {
               icon: const Icon(Icons.add))
         ],
       ),
-      body: const PlacesList(places: []),
+      body: PlacesList(places: userPlaces),
     );
   }
 }
