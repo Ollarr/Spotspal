ae3f87c31a0403d4e1a55b9eb8a9824e1ffda54e
Oluwasegun <oluyedis@gmail.com>
2023-11-02 10:57:19 +0100

    forwarded the function for saved image to the image input custom widget

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spotspal/providers/user_places.dart';
@@ -14,14 +16,18 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final titleController = TextEditingController();
+  File? selectedImage;
 
   void savePlace() {
     final enteredTitle = titleController.text;
 
-    if (enteredTitle == null || enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || selectedImage == null) {
       return;
     }
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, selectedImage!);
+
     Navigator.of(context).pop();
   }
 
@@ -47,7 +53,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 style: TextStyle(
                   color: Theme.of(context).colorScheme.onBackground,
                 )),
-            const ImageInput(),
+            ImageInput(
+              onPickedImage: (image) {
+                selectedImage = image;
+              },
+            ),
             const SizedBox(
               height: 16,
             ),
