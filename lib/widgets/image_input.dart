6636d9a0351916bef3b51998b404f1aeee9b7231
Oluwasegun <oluyedis@gmail.com>
2023-11-02 10:56:56 +0100

    forwarded the function for saved image to the image input custom widget

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({super.key});
+  const ImageInput({super.key, required this.onPickedImage});
+
+  final void Function(File image) onPickedImage;
 
   @override
   State<ImageInput> createState() {
@@ -10,10 +15,41 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void takePicture() {}
+  File? selectedImage;
+
+  void takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      selectedImage = File(pickedImage.path);
+    });
+    widget.onPickedImage(selectedImage!);
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+        onPressed: () {},
+        icon: const Icon(Icons.camera),
+        label: const Text("Select ot take a picture"));
+
+    if (selectedImage != null) {
+      content = GestureDetector(
+        onTap: takePicture,
+        child: Image.file(
+          selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
         border: Border.all(
@@ -23,10 +59,7 @@ class _ImageInputState extends State<ImageInput> {
       ),
       height: 250,
       width: double.infinity,
-      child: TextButton.icon(
-          onPressed: () {},
-          icon: const Icon(Icons.camera),
-          label: const Text("Select ot take a picture")),
+      child: content,
     );
   }
 }
